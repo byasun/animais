41b836fb670354af2bb33e327e8dfedd9152bfdc
Anna Clara Gomes <102702295+byasun@users.noreply.github.com>
2022-11-22 21:57:18 -0300

    Update Cachorro.java

--- Cachorro.java
+++ Cachorro.java
@@ -1,12 +1,26 @@
-public class Cachorro extends Animais{
-    public Cachorro(String nome, double peso) {
+public class Cachorro extends Animais {
+    public Cachorro(String nome, double peso, String raca) {
+
         super(nome, peso);
+        this.raca =raca;
     }
+
     public String raca;
+
     public String getRaca() {
+
         return raca;
     }
+
     public void setRaca(String raca) {
+
         this.raca = raca;
+    }
+
+    @Override
+    public String toString() {
+        return "Cachorro{" +
+                "raca='" + raca + '\'' +
+                '}';
     }
 }
