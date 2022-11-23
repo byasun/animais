e0a220f8e5eb48025cab525069ad81e6e045ea8e
Anna Clara Gomes <102702295+byasun@users.noreply.github.com>
2022-11-22 21:58:15 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
-class Main{
+class Main {
     public static void main(String[] args) {
-
+        Peixe peixe1 = new Peixe("alfred", 500, "rio");
+        Cachorro dog = new Cachorro("alan", 4, "husky");
+        System.out.println("O nome do peixe é: " + peixe1.getNome()+ "\nEle pesa: " + peixe1.getPeso() + "\nO habitat dele é o " + peixe1.getHabitat());
+        System.out.println("\n");
+        System.out.println("O nome do cachorro é: " + dog.getNome()+ "\nEle pesa: " + dog.getPeso() + "kilos \nA raça dele é o " + dog.getRaca());
     }
 }
